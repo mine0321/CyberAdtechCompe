46b737650c78429d6aa3f3b9b69e62a74514efe7
taguro <tagurodevelop@yahoo.co.jp>
2016-02-19 18:45:57 +0900

    healthHandler

--- server.py
+++ server.py
@@ -3,15 +3,18 @@ import tornado.web
 import tornado.httpserver
 from tornado.web import gen
 import time
-
+from Dsp.Dsp1 import DSP1
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        
+        self.write("hello world")
+
+class HealthHandler(tornado.web.RequestHandler):
+    def get(self):
         self.write("hello world")
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/", MainHandler),(r"/health", HealthHandler)
 ])
 
 if __name__ == "__main__":
