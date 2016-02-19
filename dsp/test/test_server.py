c0e727bfd7d71407d1aeed24a48c9172ceeccb70
taguro <tagurodevelop@yahoo.co.jp>
2016-02-19 22:56:24 +0900

    MainHandlerのユニットテストを作成

---
+++ dsp/test/test_server.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2016/02/19
+
+@author: develop
+'''
+import unittest
+import server
+import tornado
+from tornado.escape import to_unicode
+from tornado.testing import AsyncTestCase, AsyncHTTPClient,gen_test
+from tornado.httpclient import HTTPError
+
+#server.pyのテスト 参考 http://yukirinmk2.hatenablog.com/entry/2014/12/21/050133
+class TestServer(AsyncTestCase):
+    @tornado.testing.gen_test
+    def test_http_fetch(self):
+        print("testMainHandler")
+        client = AsyncHTTPClient(self.io_loop)
+        response = yield client.fetch("http://localhost:8080")
+        # Test contents of response
+        self.assertEqual("hello world", response.body)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testMainHandler']
+    unittest.main()
