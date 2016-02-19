64ef9d7eb36f4d29479c20b8af1e9bfdce57a22d
root <root@ip-172-31-6-198.ap-northeast-1.compute.internal>
2016-02-19 09:38:45 +0000

    :change
    test delete

--- server.py
+++ server.py
@@ -1,17 +1,27 @@
-#!/bin/env python
-# -*- coding: utf-8 -*-
 import tornado.ioloop
 import tornado.web
+import tornado.httpserver
+from tornado.web import gen
 import time
 
+
 class MainHandler(tornado.web.RequestHandler):
+    def slp(self, *args, **kwargs):
+        #time.sleep(0.02)
+        return kwargs['callback']()
+
+    @tornado.web.asynchronous
+    @gen.coroutine
     def get(self):
-        self.write("Hello, world")
+        t = yield gen.Task(self.slp)
+        self.write("hello world")
 
 application = tornado.web.Application([
     (r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
-    application.listen(8080)
-    tornado.ioloop.IOLoop.instance().start()
+    server = tornado.httpserver.HTTPServer(application)
+    server.bind(8080)
+    server.start(0)
+    tornado.ioloop.IOLoop.current().start()
