ea2a07cf973643e60f0035ca4228e93e93249ed4
mine0321 <nagamine@yairilab.net>
2016-02-20 15:46:56 +0900

    change 'nan' to np.nan

--- sample_notebook/CTR_EstimationModel.py
+++ sample_notebook/CTR_EstimationModel.py
@@ -3,6 +3,7 @@
 
 import xgboost as xgb
 from sklearn.externals import joblib
+import numpy as np
 
 class CTR_Estimation(object):
     """docstring for CTR_Estimation"""
@@ -19,7 +20,7 @@ class CTR_Estimation(object):
         self.modnum = 3
 
     def estimation(self, sample_dict):
-        if sample_dict['floorPrice'] == 'nan':
+        if sample_dict['floorPrice'] == np.nan:
             sample_dict['floorPrice'] = 0
         if sample_dict['device'] == 'iOS':
             test_sample = xgb.DMatrix(
