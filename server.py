7ad9db5e2489b471f7bcdb21913a12f182db3190
root <root@ip-172-31-10-12.ap-northeast-1.compute.internal>
2016-02-20 02:00:28 +0000

    :add
    json get

--- server.py
+++ server.py
@@ -2,13 +2,16 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpserver
 import redis
+import json
 from tornado.web import gen
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        r = redis.StrictRedis(host='elc-002.wlnxen.0001.apne1.cache.amazonaws.com', port=6379, db=0)
-        foo = r.get('foo')
-        self.write(foo)
+    def post(self):
+        # r = redis.StrictRedis(host='elc-002.wlnxen.0001.apne1.cache.amazonaws.com', port=6379, db=0)
+        # foo = r.get('foo')
+        data = tornado.escape.json_decode(self.request.body)
+        print(data)
+        self.write(data)
 
 class HealthHandler(tornado.web.RequestHandler):
     def get(self):
