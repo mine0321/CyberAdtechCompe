db0f87e51359f80166ec8342d360922fe36d32e0
柳 耕太 <yanagikouta0602@gmail.com>
2016-02-20 19:07:51 +0900

    no message

--- server.py
+++ server.py
@@ -6,12 +6,12 @@ from tornado.escape import json_encode
 
 import redis
 import json
+from torndb import Connection
 
 from sample_notebook.CTR_EstimationModel import CTR_Estimation
 
 class MainHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    @gen.coroutine
+
     def post(self):
         r = redis.StrictRedis(host='elc-002.wlnxen.0001.apne1.cache.amazonaws.com', port=6379, db=0)
         cpc = int(r.get('cpc'))
@@ -20,11 +20,12 @@ class MainHandler(tornado.web.RequestHandler):
         document = CTR_Estimation()
         ctr = document.estimation(data)
         advertiserId = 1
+        price = cpc * ctr
 
-        gen.Task(self.insert(data))
-        self.responseJson(data['id'], cpc, ctr, advertiserId)
+        insert(data, price, advertiserId)
+        self.responseJson(data['id'], price, advertiserId)
 
-    def responseJson(self, id, cpc, ctr, advertiserId):
+    def responseJson(self, data, price, advertiserId):
         self.set_header('Content-Type', 'application/json')
         self.set_status(200)
         json = {
@@ -33,10 +34,11 @@ class MainHandler(tornado.web.RequestHandler):
             'advertiserId': advertiserId
         }
         self.write(json_encode(json))
+            db = torndb.Connection("dataallin.ca6eqefmtfhj.ap-northeast-1.rds.amazonaws.com:3306", "db", user='team_f', password='password')
+            db.execute("INSERT INTO posts (id, floor_price, site, device, user, advertiser_id, bit_price) VALUES (%s, %s, %s, %s, %s, %s ,%s)", data['id'], data['floorPrice'], data['site'], data['device'], data['user'], advertiserId, price)
 
-    def insert(self, *args, **kwargs, data):
-        print(data)
-        return kwargs['callback']()
+    def insert(self, data, price, advertiserId):
+
 
 
 class HealthHandler(tornado.web.RequestHandler):
