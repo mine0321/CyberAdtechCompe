6f95e09299ee15ebfe0f12d6724964f6f757da8d
rihito0907 <rihito0907@gmail.com>
2016-02-21 05:50:18 +0900

    make update handler

--- calc_server.py
+++ calc_server.py
@@ -3,26 +3,30 @@ import tornado.web
 import tornado.httpserver
 import redis
 import json
-import torndb
 from tornado.web import gen
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        db = torndb.Connection("dataallin.ca6eqefmtfhj.ap-northeast-1.rds.amazonaws.com:3306", "db", user='team_f', password='password')
-	rows = db.query("select Id,Title from post")
-	self.write("ssss")
+from sqlalchemy import create_engine, exc
 
-class WinHandler(tornado.web.RequestHandler):
+REDIS_HOST = 'elc-002.wlnxen.0001.apne1.cache.amazonaws.com'
+DATABASE = 'mysql://team_f:password@dataallin.ca6eqefmtfhj.ap-northeast-1.rds.amazonaws.com:3306/db'
+
+class HealthHandler(tornado.web.RequestHandler):
+    def get(self):
+        print("Hello")
+
+class UpdateHandler(tornado.web.RequestHandler):
     def get(self):
         data = tornado.escape.json_decode(self.request.body)
         self.write(data)
 
 application = tornado.web.Application([
-    (r"/", MainHandler),(r"/win", WinHandler)
+    (r"/health", HealthHandler),
+    (r"/update", UpdateHandler)
 ],
 )
 
 if __name__ == "__main__":
+    engine = create_engine(DATABASE, pool_size=20, max_overflow=0)
     server = tornado.httpserver.HTTPServer(application)
     server.bind(8080)
     server.start(0)
