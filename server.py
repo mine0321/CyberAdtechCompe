9b1ff9cab7b6ba3bed4e3896087383337c01841f
rihito0907 <rihito0907@gmail.com>
2016-02-21 13:10:21 +0900

    e

--- server.py
+++ server.py
@@ -95,5 +95,5 @@ if __name__ == "__main__":
     document = CTR_Estimation()
     server = tornado.httpserver.HTTPServer(application)
     server.bind(8080)
-    server.start(2)
+    server.start(0)
     tornado.ioloop.IOLoop.current().start()
